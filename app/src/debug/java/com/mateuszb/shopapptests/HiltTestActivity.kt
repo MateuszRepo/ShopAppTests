ee0013a2fa294bac4fdfd64c909c4d7009d369c4
Mateusz <mateuszbe1993@o2.pl>
2020-11-16 19:37:11 +0100

    Creating HiltExtension for Fragments in order to instantiate Hilt fragment container

---
+++ app/src/debug/java/com/mateuszb/shopapptests/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.mateuszb.shopapptests
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
