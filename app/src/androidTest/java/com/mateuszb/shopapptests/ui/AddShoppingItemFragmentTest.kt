ae2d3efa7f24d661d44b40497a9dd8de154261cc
Mateusz <mateuszbe1993@o2.pl>
2020-11-18 23:31:50 +0100

    Test Adding ShoppingItem

--- app/src/androidTest/java/com/mateuszb/shopapptests/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/mateuszb/shopapptests/ui/AddShoppingItemFragmentTest.kt
@@ -1,14 +1,21 @@
 package com.mateuszb.shopapptests.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.mateuszb.shopapptests.R
+import com.mateuszb.shopapptests.data.local.ShoppingItem
+import com.mateuszb.shopapptests.getOrAwaitValue
 import com.mateuszb.shopapptests.launchFragmentInHiltContainer
+import com.mateuszb.shopapptests.repositories.FakeShoppingRepositoryAndroidTest
+import com.mateuszb.shopapptests.ui.viewmodels.ShoppingViewModel
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -17,17 +24,41 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
 class AddShoppingItemFragmentTest {
+
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShopAppFragmentFactory
 
     @Before
     fun setup(){
         hiltRule.inject()
+    }
+
+    @Test
+    fun clickOnInsertIntoDB_shoppingItemInsertedIntoDB() {
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(fragmentFactory = fragmentFactory) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("Item name"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("7"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("7.0"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem(null, "Item name", 7, 7.7f, "" ))
     }
 
     @Test
--- app/src/main/java/com/mateuszb/shopapptests/ui/AddShoppingItemFragment.kt
+++ app/src/main/java/com/mateuszb/shopapptests/ui/AddShoppingItemFragment.kt
@@ -4,19 +4,36 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import com.mateuszb.shopapptests.R
+import com.mateuszb.shopapptests.others.Resource
 import com.mateuszb.shopapptests.ui.viewmodels.ShoppingViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
+import javax.inject.Inject
 
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+) : Fragment(R.layout.fragment_add_shopping_item) {
 
     lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        subscribeToObservers()
+
+        btnAddShoppingItem.setOnClickListener {
+            viewModel.insertShoppingItem(
+                etShoppingItemName.text.toString(),
+                etShoppingItemAmount.text.toString(),
+                etShoppingItemPrice.text.toString()
+            )
+        }
 
         ivShoppingImage.setOnClickListener {
             findNavController().navigate(AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment())
@@ -29,5 +46,37 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.currentImageUrl.observe(viewLifecycleOwner, Observer {
+            glide.load(it).into(ivShoppingImage)
+        })
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { result ->
+                when(result){
+                    is Resource.Success -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            "Shopping item added successfully",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+
+                    is Resource.Error -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            result.message ?: "Unknown error",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    //Writing to Room database doesn't require dialog because it is operation fast
+                    is Resource.Loading -> {
+                        /* NO_OP */
+                    }
+                }
+            }
+        })
     }
 }
